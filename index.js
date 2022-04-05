ab41d14ca9c7bc0de6d057b96628bc1664db8867
mardenmoura <moura.dev@outlook.com>
2022-04-05 01:13:52 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let olderPersonName = prompt("Digite o nome da pessa mais velha")
+let olderPersonAge = prompt("Digite a idade da pessoa mais velha")
+
+let youngerPersonName = prompt("Digite o nome da pessoa mais nova")
+let youngerPersonAge = prompt("Digite a idade da pessoa mais nova")
+
+let ageDifference = olderPersonAge - youngerPersonAge
+
+alert(
+    "Pessoa mais velha: " + olderPersonName + "\nIdade: " + olderPersonAge +
+    "\n\nPessoa mais nova: " + youngerPersonName + "\nIdade: " + youngerPersonAge +
+    "\n\nDiferença de idade entre as duas pessoas: " + ageDifference
+)
+
+let contiue = confirm("Clique em OK para retornar e CANCEL para terminar.")
+let tanks = alert("Obrigado!")
+if (contiue === true) {
+
+    let olderPersonName = prompt("Digite o nome da pessa mais velha")
+    let olderPersonAge = prompt("Digite a idade da pessoa mais velha")
+
+    let youngerPersonName = prompt("Digite o nome da pessoa mais nova")
+    let youngerPersonAge = prompt("Digite a idade da pessoa mais nova")
+
+    let ageDifference = olderPersonAge - youngerPersonAge
+
+    alert(
+        "Pessoa mais velha: " + olderPersonName + "\nIdade: " + olderPersonAge +
+        "\n\nPessoa mais nova: " + youngerPersonName + "\nIdade: " + youngerPersonAge +
+        "\n\nDiferença de idade entre as duas pessoas: " + ageDifference
+    )
+
+} else (tanks)
